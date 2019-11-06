15c341734cebe4e3bf4655f7114223db309b83aa
SyogoShibuya <hotococoa0204@icloud.com>
2019-11-07 03:13:21 +0900

    修正

--- models/resnet50_fine.py
+++ models/resnet50_fine.py
@@ -28,14 +28,13 @@ class ResNet50_Fine(chainer.Chain):
             self.base = L.ResNet50Layers()
             self.fc = L.Linear(None, output)
 
-    def __call__(self, x):
+    def __call__(self, x, x_st):
         h = self.base(x, layers=['res5'])['res5']
         self.cam = h
         h = _global_average_pooling_2d(h)
         h = self.fc(h)
-        
-        x_z = Center_Crop(x)
-        h_z = self.base(x_z, layers=['res5'])['res5']
+
+        h_z = self.base(x_st, layers=['res5'])['res5']
         h_z = _global_average_pooling_2d(h_z)
         h_z = self.fc(h_z)
 
@@ -47,16 +46,16 @@ class StabilityClassifer(L.Classifier):
                  accfun=F.accuracy,):
         super(StabilityClassifer, self).__init__(predictor=predictor, lossfun=lossfun, accfun=accfun)
 
-    def forward(self, x, t):
+    def forward(self, x, t, x_st):
 
         if not chainer.config.train:
-            self.y ,self.y_z = self.predictor(x)
+            self.y ,self.y_z = self.predictor(x, x_st)
             self.loss = self.lossfun(self.y, t)
         else:
-            self.y, self.y_z= self.predictor(x)
+            self.y, self.y_z = self.predictor(x, x_st)
             l1 = self.lossfun(self.y, t)
             self.l2 = compute_KLd(self.y, self.y_z)
-            self.loss = l1 + (0.5 * self.l2)
+            self.loss = l1 + (0.2 * self.l2)
 
         reporter.report({'loss': self.loss}, self)
         if self.compute_accuracy:
@@ -71,11 +70,3 @@ def compute_KLd(p, q):
         q = F.softmax(q, axis=1)
         return F.sum(p * (F.log(p + 1e-16) - F.log(q + 1e-16)))
 
-
-def Center_Crop(x):
-    for i in range(x.shape[0]):
-        h = x.shape[2]
-        r = random.randint(h//2, h)
-        a = transforms.center_crop(x[i], (224, 224))
-        x[i] = transforms.resize(a, (h, h))
-    return x
